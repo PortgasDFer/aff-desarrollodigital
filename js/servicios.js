8a2c9d9c039e89540373aab5d9fe63358e548617
Georlouis <georlouis@gmail.com>
2026-05-20 12:13:44 -0600

    Servicios

---
+++ js/servicios.js
@@ -0,0 +1,14 @@
+
+document.querySelectorAll('.faq-item').forEach(item => {
+
+  const button =
+    item.querySelector('.faq-question');
+
+  button.addEventListener('click', () => {
+
+    item.classList.toggle('active');
+
+  });
+
+});
+
